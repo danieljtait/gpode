b96ad16673a6820da57c260e70772ad1b0fa92e9
danieljtait <tait.djk@gmail.com>
2017-03-27 15:38:26 +0100

    Basic example of GP class

--- GP-ODE/example1.py
+++ GP-ODE/example1.py
@@ -1,1 +1,16 @@
 import numpy as np
+import matplotlib.pyplot as plt
+
+from gp_src import ksqexp, GaussianProcess
+
+
+gp1 = GaussianProcess(ksqexp, 5.)
+tt = np.linspace(0., 1., 11)
+
+rf = gp1.sim(tt)
+
+fig = plt.figure()
+ax = fig.add_subplot(111)
+ax.plot(tt, rf, 'o')
+
+plt.show()
