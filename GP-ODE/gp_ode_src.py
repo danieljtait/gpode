8dbf9b4050e2c20172e7ab848578bbaa5a3df4f1
danieljtait <tait.djk@gmail.com>
2017-03-28 19:42:13 +0100

    Basic latent Gaussian Process ODE

---
+++ GP-ODE/gp_ode_src.py
@@ -0,0 +1,30 @@
+import numpy
+
+#
+# ToDo - Add F pars maps
+class LatentVelocity:
+    def __init__(self, F, latentGPs):
+        self.dim = len(latentGPs) # Expects latentGPs as a list
+        self.latentGPs = latentGPs
+        self.F = F
+        
+    # Calls the simulate method of the pth
+    # latent Gaussian process
+    def sim_latent(self, p, tp, set=False):
+        r = self.latentGPs.sim(tp)
+        if set:
+            self.set_latent(p, tp, r)
+
+    # Set the conditioned points of
+    # the pth latent Gaussian process
+    def set_latent(self, p, tp, xp):
+        self.latentGPs[p].interp_fit(tp, xp)
+
+    # Potential to add an additional sensitivity
+    # dependency structure to the latent GPs
+    def dXdt(self,X,t=0,indep=True):
+        eta = np.array([gp.interp_evalf(t) for gp in self.latentGPs])
+        return self.F(X) + eta
+
+    
+        
